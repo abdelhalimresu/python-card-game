9d8397c6217b19016d902ba38766602804ae876a
abdelhalimresu <abdelhalim.resu@gmail.com>
2022-11-10 12:40:49 +0100

    Card class

---
+++ classes/card.py
@@ -0,0 +1,12 @@
+class Card:
+    
+    suits = ['♣', '♦', '♥', '♠']
+    ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+
+    def __init__(self, suit, rank):
+
+        self.suit = suit
+        self.rank = rank
+    
+    def __str__(self):
+        return self.suits[self.suit] + self.ranks[self.rank]
