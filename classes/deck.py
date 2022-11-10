a9ed7351a6a377a28b67bce2b3f41eac4ea803fa
abdelhalimresu <abdelhalim.resu@gmail.com>
2022-11-10 15:12:14 +0100

    Add Deck class

---
+++ classes/deck.py
@@ -0,0 +1,29 @@
+from card import Card
+from random import shuffle
+
+class Deck:
+
+    def __init__(self):
+        self.cards = []
+        for i in range(4):
+            for j in range(13):
+                self.add_card(Card(i, j))
+
+    def __str__(self):
+        str_card = ''
+        for card in self.cards:
+            str_card += ' | ' + str(card)
+        str_card += ' | '
+        return str_card
+    
+    def __len__(self):
+        return len(self.cards)
+
+    def add_card(self, card):
+        self.cards.append(card)
+
+    def shuffle(self):
+        shuffle(self.cards)
+
+    def pop_card(self):
+        return self.cards.pop(0)
